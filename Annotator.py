98b66c68c228601f6374379b882f87e2d703c075
lukasiktar <luka.siktar11@gmail.com>
2024-10-15 14:54:56 +0200

    nez ni kak sam ovo uspio napravit, ispravljeno..

--- Annotator.py
+++ Annotator.py
@@ -4,9 +4,9 @@ image: str = 'car.png'
 annotation_image: str = '001'
 
 if rectangle:
-    from Segmentator_point import segment_using_mouse
-    segment_using_mouse(image_path=f'images/{image}', annotated_image_name=annotation_image)
-
-else:
     from Segmentator_rectangle import segment_using_rectangle
     segment_using_rectangle(image_path=f'images/{image}', annotated_image_name=annotation_image)
+
+else:
+    from Segmentator_point import segment_using_mouse
+    segment_using_mouse(image_path=f'images/{image}', annotated_image_name=annotation_image)
