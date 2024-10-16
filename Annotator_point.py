9997acabfe4b2d604ccc3ee7e50a8900e4da35f1
Luka Matijevic <luka.matijevic45@gmail.com>
2024-10-16 13:11:30 +0200

    moved to Annotator

--- Annotator_point.py
+++
@@ -1,4 +0,0 @@
-from Segmentator_point import segment_using_mouse
-
-
-segment_using_mouse(image_path="images/car.png", annotated_image_name="001")
--- Annotator_rectangle.py
+++
@@ -1,3 +0,0 @@
-from Segmentator_rectangle import segment_using_rectangle
-
-segment_using_rectangle(image_path="images/car.png",annotated_image_name="001")
